8f77ed6faf967e163fddc082d634480787c909fe
ValentynaVel <110202655+ValentynaVel@users.noreply.github.com>
2022-08-07 23:03:31 +0100

    icon update

--- src/script.js
+++ src/script.js
@@ -63,10 +63,16 @@ function showTempr(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   conditionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function city(event) {
